0e55cdd67125616c3d6a8eb1ebc2bfb0d9e33361
Unknown <up201603404@fe.up.pt>
2018-03-01 19:11:43 +0000

    O 3 ta done
    falta o extra só

--- WebCGF.tp2/tp2/MyTable.js
+++ WebCGF.tp2/tp2/MyTable.js
@@ -1,49 +1,54 @@
 /**
- * MyTable
- * @constructor
- */
- class MyTable extends CGFobject
- {
-	constructor(scene) 
-	{
+* MyTable
+* @param gl {WebGLRenderingContext}
+* @constructor
+*/
+
+class MyTable extends CGFobject {
+	constructor(scene) {
 		super(scene);
+		this.cube = new MyUnitCubeQuad(this.scene)
+		this.leg = new MyLegs(this.scene);
 
-		this.myUnitCubeQuad = new MyUnitCubeQuad(this.scene);
+
 	};
 
-	display() 
-	{
-		// legs
+	display() {
 		this.scene.pushMatrix();
-		this.scene.translate(2, 3.5 / 2, 1);
-		this.scene.scale(0.3, 3.5, 0.3);
-		this.myUnitCubeQuad.display();
-		this.scene.popMatrix();
-
+		this.scene.translate(0,3.65,0);//altura das pernas 3.5 + metade da altura do tampo 0.3/2=0.15
 		this.scene.pushMatrix();
-		this.scene.translate(2, 3.5 / 2, -1);
-		this.scene.scale(0.3, 3.5, 0.3);
-		this.myUnitCubeQuad.display();
-		this.scene.popMatrix();
-
-		this.scene.pushMatrix();
-		this.scene.translate(-2, 3.5 / 2, 1);
-		this.scene.scale(0.3, 3.5, 0.3);
-		this.myUnitCubeQuad.display();
-		this.scene.popMatrix();
-
-		this.scene.pushMatrix();
-		this.scene.translate(-2, 3.5 / 2, -1);
-		this.scene.scale(0.3, 3.5, 0.3);
-		this.myUnitCubeQuad.display();
-		this.scene.popMatrix();
-
-		// table top
-		this.scene.pushMatrix();
-		this.scene.translate(0, 3.5, 0);
 		this.scene.scale(5, 0.3, 3);
-		this.myUnitCubeQuad.display();
+		this.cube.display();
 		this.scene.popMatrix();
-	};
- };
+		this.scene.popMatrix();
+		
 
+
+		this.scene.pushMatrix();
+		this.scene.translate(-2.35,1.75,-1.35); //metade do x do tampo 5/2 = 2.5 - metade da largura da perna 0.3/2= 0.15
+		this.leg.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(-2.35,1.75,1.35);
+		this.leg.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(2.35,1.75,1.35);
+		this.leg.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(2.35,1.75,-1.35);
+		this.leg.display();
+		this.scene.popMatrix();
+
+
+	};
+
+
+
+
+
+};
--- WebCGF.tp2/tp2/MyUnitCubeQuad.js
+++ WebCGF.tp2/tp2/MyUnitCubeQuad.js
@@ -1,57 +1,74 @@
 /**
  * MyUnitCubeQuad
+ * @param gl {WebGLRenderingContext}
  * @constructor
  */
+
 class MyUnitCubeQuad extends CGFobject
 {
 	constructor(scene) 
 	{
 		super(scene);
-
-		this.quad = new MyQuad(this.scene);
+		this.quad = new MyQuad(this.scene)
+		this.quad.initBuffers();
 	};
 
-	display() 
-	{
-		// front face
+	display(){
+		var cvs = Math.PI/180;
 		this.scene.pushMatrix();
-		this.scene.translate(0, 0, 0.5);
-		this.quad.display();
+		this.scene.translate(0,0,0.5);
+		this.quad.display();//front face
 		this.scene.popMatrix();
 
-		// back face
+
 		this.scene.pushMatrix();
-		this.scene.rotate(180 * degToRad, 1, 0, 0);
-		this.scene.translate(0, 0, 0.5);
-		this.quad.display();
+		this.scene.translate(0,0,-0.5);
+		this.scene.pushMatrix();
+		this.scene.rotate(cvs*180, 0, 3,0);
+		this.quad.display();//back face
+		this.scene.popMatrix();
 		this.scene.popMatrix();
 
-		// top face
+
 		this.scene.pushMatrix();
-		this.scene.rotate(-90 * degToRad, 1, 0, 0);
-		this.scene.translate(0, 0, 0.5);
+		this.scene.translate(0.5,0,0);
+		this.scene.pushMatrix()
+		this.scene.rotate(90*cvs, 0,1,0);
 		this.quad.display();
+		this.scene.popMatrix();
 		this.scene.popMatrix();
 
-		// back face
+
 		this.scene.pushMatrix();
-		this.scene.rotate(90 * degToRad, 1, 0, 0);
-		this.scene.translate(0, 0, 0.5);
+		this.scene.translate(-0.5,0,0);
+		this.scene.pushMatrix();
+		this.scene.rotate(-90*cvs, 0,1,0);
 		this.quad.display();
+		this.scene.popMatrix();
 		this.scene.popMatrix();
 
-		// right face
 		this.scene.pushMatrix();
-		this.scene.rotate(-90 * degToRad, 0, 1, 0);
-		this.scene.translate(0, 0, 0.5);
+		this.scene.translate(0,-0.5,0);
+		this.scene.pushMatrix();
+		this.scene.rotate(90*cvs, 1,0,0);
 		this.quad.display();
+		this.scene.popMatrix();
 		this.scene.popMatrix();
 
-		// left face
 		this.scene.pushMatrix();
-		this.scene.rotate(90 * degToRad, 0, 1, 0);
-		this.scene.translate(0, 0, 0.5);
+		this.scene.translate(0,0.5,0);
+		this.scene.pushMatrix();
+		this.scene.rotate(-90*cvs, 1,0,0);
 		this.quad.display();
 		this.scene.popMatrix();
-	};
+		this.scene.popMatrix();
+
+
+
+};
+
+
+
+
+	
 };
