9781ef75e912e3d3ceaba5c1a0bdd5c5efca17de
Unknown <up201603404@fe.up.pt>
2018-05-16 01:25:23 +0100

    nao consegui meter o cubo no meio das rodas
    Nao tou a prcebr porqe

--- Project/proj/MyVehicle.js
+++ Project/proj/MyVehicle.js
@@ -9,42 +9,55 @@ class MyVehicle extends CGFobject {
     constructor(scene) {
         super(scene);
 
-        this.tronco = 
+        this.tronco = new MyPrism(scene, 4, 20);
         this.wheel = new MyWheel(scene);
 
     }
 
-    display(){
+    display() {
 
         this.scene.pushMatrix();
-        this.scene.rotate(0,1,0,Math.PI);
+        this.scene.rotate(Math.PI, 0, 2, 0);
         this.scene.pushMatrix();
-        this.scene.translate(2,2,-2);
+        this.scene.translate(-7, 2, 2);
+        this.wheel.display();
+        this.scene.popMatrix();
+      
+        //another 
+
+        this.scene.pushMatrix();
+        this.scene.translate(-2, 2, 2);
+        this.wheel.display();
+        this.scene.popMatrix();
+        this.scene.popMatrix();
+
+        //
+        this.scene.pushMatrix();
+        this.scene.translate(2, 2, 2);
         this.wheel.display();
         this.scene.popMatrix();
 
         //another 
 
         this.scene.pushMatrix();
-        this.scene.translate(7,2,-2);
-        this.wheel.display();
-        this.scene.popMatrix();
-        this.scene.popMatrix();
-        
-//
-        this.scene.pushMatrix();
-        this.scene.translate(2,2,2);
-        this.wheel.display();
-        this.scene.popMatrix();
-
-        //another 
-
-        this.scene.pushMatrix();
-        this.scene.translate(7,2,2);
+        this.scene.translate(7, 2, 2);
         this.wheel.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        
+        //  this.scene.rotate(0.5*Math.PI, 0,1,0);
+
+
+
+        this.scene.pushMatrix();
+        this.scene.scale(1.7, 2, 3);
+        this.scene.pushMatrix();
+        this.scene.rotate(0.25 * Math.PI, 0, 0, 3);
+        // queria so meter o cubo entre as rodas
+        this.scene.translate(0, 0, 0);
+        this.tronco.display();
+        this.scene.popMatrix();
+        this.scene.popMatrix();
+        this.scene.popMatrix();
     }
 }
