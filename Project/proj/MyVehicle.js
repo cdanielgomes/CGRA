e395b6e1a229c0267d52d0b1d687f6ed39354bb4
Tiago Cardoso <33624471+tpcred@users.noreply.github.com>
2018-05-16 10:29:43 +0100

    carro cansado

--- Project/proj/MyVehicle.js
+++ Project/proj/MyVehicle.js
@@ -9,7 +9,7 @@ class MyVehicle extends CGFobject {
     constructor(scene) {
         super(scene);
 
-        this.tronco = new MyPrism(scene, 4, 20);
+        this.tronco = new MyUnitCubeQuad(scene);
         this.wheel = new MyWheel(scene);
 
     }
@@ -17,30 +17,33 @@ class MyVehicle extends CGFobject {
     display() {
 
         this.scene.pushMatrix();
+        this.scene.scale(0.5,0.5,0.5);
         this.scene.rotate(Math.PI, 0, 2, 0);
         this.scene.pushMatrix();
-        this.scene.translate(-7, 2, 2);
+        this.scene.translate(2.5, 1, 2);
         this.wheel.display();
         this.scene.popMatrix();
       
         //another 
 
         this.scene.pushMatrix();
-        this.scene.translate(-2, 2, 2);
+        this.scene.translate(-2, 1, 2);
         this.wheel.display();
         this.scene.popMatrix();
         this.scene.popMatrix();
 
         //
         this.scene.pushMatrix();
-        this.scene.translate(2, 2, 2);
+        this.scene.scale(0.5,0.5,0.5);
+        this.scene.translate(2, 1, 2);
         this.wheel.display();
         this.scene.popMatrix();
 
         //another 
 
         this.scene.pushMatrix();
-        this.scene.translate(7, 2, 2);
+        this.scene.scale(0.5,0.5,0.5);
+        this.scene.translate(-2.5, 1, 2);
         this.wheel.display();
         this.scene.popMatrix();
 
@@ -50,11 +53,12 @@ class MyVehicle extends CGFobject {
 
 
         this.scene.pushMatrix();
-        this.scene.scale(1.7, 2, 3);
+        this.scene.translate(0, 0.5, 0);
+        this.scene.scale(4, 1.2, 2);
+        this.scene.translate(0, 0.3, 0);
         this.scene.pushMatrix();
-        this.scene.rotate(0.25 * Math.PI, 0, 0, 3);
+        //this.scene.rotate(0.25 * Math.PI, 0, 0, 3);
         // queria so meter o cubo entre as rodas
-        this.scene.translate(0, 0, 0);
         this.tronco.display();
         this.scene.popMatrix();
         this.scene.popMatrix();
